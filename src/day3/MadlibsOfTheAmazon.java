2b19a52c65969d9d8550095f1774bc5e79a5f81f
PotatoBob <league@WTS11.attlocal.net>
2016-04-08 15:04:10 -0700

    Day 5
    Scary maze

--- src/day3/MadlibsOfTheAmazon.java
+++ src/day3/MadlibsOfTheAmazon.java
@@ -7,24 +7,24 @@ public class MadlibsOfTheAmazon {
 
 		// Put this sentence in a pop up:
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
-
+		JOptionPane.showMessageDialog(null, "If you find yourself having to cross a piranha-infested river, here's how to do it...");
 		// Get the user to enter an adjective
-
+		String adjective= JOptionPane.showInputDialog("Enter an adjective");
 		// Get the user to enter a type of liquid
-
+		String liquid= JOptionPane.showInputDialog("Enter a type of liquid");
 		// Get the user to enter a body part
-
+		String body= JOptionPane.showInputDialog("Enter a body part");
 		// Get the user to enter a verb
-
+		String verb= JOptionPane.showInputDialog("Enter a verb");
 		// Get the user to enter a place
-
+		String place= JOptionPane.showInputDialog("Enter a place");
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
 		// night. Piranhas are attracted to fresh [type of liquid] and will most
 		// likely take a bite out of your [body part] if you [verb]. Whatever
 		// you do, if you have an open wound, try to find another way to get
 		// back to the [place]. Good luck!
-		
+		//String adjective= JOptionPane.showMessageDialog(null, Piranh);
 		// Make a pop-up for the final story. You can use \n to go to the next line.
 		
 
--- src/day5/ScaryMaze.java
+++ src/day5/ScaryMaze.java
@@ -12,24 +12,25 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 	
 	BufferedImage maze;
-	final int frameWidth = 600;
-	final int frameHeight = 400;
+	final int frameWidth = 800;
+	final int frameHeight = 600;
 
 	ScaryMaze() throws Exception {
 		//1. Use this online tool to make a maze image and drop it into your day5 package: http://pixlr.com/editor/
-		maze = ImageIO.read(getClass().getResource("maze.png"));
+		maze = ImageIO.read(getClass().getResource("Maze.jpg"));
 		//2. set the mouse pointer to the start of your maze using:
 		//new Robot().mouseMove(xPosition, yPosition)
-		
+		new Robot().mouseMove(80, 95);
 		//3. add a mouse motion listener using:
 		//addMouseMotionListener(this)
-		
+		addMouseMotionListener(this);
 	}
 
 	@Override
@@ -38,15 +39,17 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		int mouseY = e.getY();
 		int mouseColor = maze.getRGB(mouseX, mouseY);
 		//4. print the mouseColor variable to see what color the mouse is touching
-
+		System.out.println(mouseColor);
 		//5. make a variable to hold the background color. 
-
+		int background = -1;
 		//6. if the mouse falls off the path (if it is on the background)
-		
+		if (mouseColor==background){
 				// call the scare method
-		
+		scare();}
 		//10. if the mouse is on the end color
-				
+		if (mouseColor==-876278){
+			JOptionPane.showMessageDialog(null, "Good job, you won!");
+		}
 				// pop up a message to tell them they won
 		
 	}
@@ -55,18 +58,18 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		System.out.println("BOO!");
 		//7. find a scary sound and put it in the day5 package where you put your maze picture. You can find a sound on freesound.org. Log in as leagueofamazing/code4life.
 		//AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav"));
-		
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource("screaming.wav"));
 		//8. play the scary sound. Hint: type "sound" and then a period.		
-		
+		sound.play();
 		//9. drop an image into your day5 package, and use the showScaryImage method to scare your victim!
-
+		showScaryImage("scary face.jpg");
 	}
 
-	private void showScaryImage(String imageName) {
+	private void showScaryImage(String ImageName) {
 		try {
-			maze = ImageIO.read(getClass().getResource(imageName));
+			maze = ImageIO.read(getClass().getResource(ImageName));
 		} catch (Exception e) {
-			System.err.println("Couldn't find this image: " + imageName);
+			System.err.println("Couldn't find this image: " + ImageName);
 		}
 		repaint();
 	}
